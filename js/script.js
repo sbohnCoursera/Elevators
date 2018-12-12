f5fbce935bb7f4f08e82028c78dc77d43b862a36
Scott Bohn <sbohn@ptd.net>
2018-12-12 12:19:51 -0500

    working on elevator object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-script.js
+function Elevator(number, floor, direction, trips, floors, maintenance) {
+
+	this.number = number;
+	this.floor = floor;
+	this.direction = direction;
+	this.trips = 0;
+	this.floors = 0;
+	this.maintenance = false;
+
+	this.updateTrips = function() {
+		return this.trips++;
+	};
+
+	this.updateFloors = function() {
+		return this.floors++;
+	};
+}
+
+function elevatorController(toFloor) {
+	
+}
