23cfe491414f6831912b794162107c99db8cb080
Scott Bohn <sbohn@ptd.net>
2018-12-12 12:34:47 -0500

    Additional updates to elevator object

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-function Elevator(number, floor, direction, trips, floors, maintenance) {
+function Elevator(number, floor, dir) {
 
 	this.number = number;
-	this.floor = floor;
-	this.direction = direction;
+	this.floor = 1;
+	this.direction = "up";
 	this.trips = 0;
 	this.floors = 0;
 	this.maintenance = false;
+
+	this.updateDirection = function() {
+		if(this.direction == "up") {
+			this.direction = "down";
+		else 
+			this.direction = "up";
+		}
+	}
+
+	this.updateFloor = function() {
+		return this.floor;
+	}
 
 	this.updateTrips = function() {
 		return this.trips++;
@@ -17,5 +29,16 @@ function Elevator(number, floor, direction, trips, floors, maintenance) {
 }
 
 function elevatorController(toFloor) {
-	
+
+}
+
+// Set max floors
+var maxFloors = 10;
+
+// Set number of elevators
+var numElevators = 3;
+
+// Initialize elevator objects
+for (var i = 0; i <= numElevators -1; i++) {
+	Things[i]
 }
